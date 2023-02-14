95565644ade44ff267fe3a4551b0a6bfe149b705
Adam Lang <adam.lang-1@hotmail.com>
2023-02-14 21:13:58 +0200

    feat: feedback array as state, passing state as prop to feedbacklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData);
+    
     return (
         <>
             <Header />
+            <div className='container'>
+                <FeedbackList feedback={feedback} />
+            </div>
         </>
     );
 }
