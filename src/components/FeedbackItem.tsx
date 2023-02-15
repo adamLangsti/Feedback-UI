69d3a30e95a90338f2b54f75447d5a3135e59b4e
Adam Lang <adam.lang-1@hotmail.com>
2023-02-15 20:14:16 +0200

    feat: passing delete function down through application

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
-export default function FeedbackItem({ item }) {
+export default function FeedbackItem({ item, handleDelete }) {
     return (
         <Card>
             <div className='num-display'>{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='purple' />
+            </button>
             <div className='text-display'>{item.text}</div>
         </Card>
     );
 }
 
 FeedbackItem.protoTypes = {
-    item: PropTypes.object.isRequired
-}
+    item: PropTypes.object.isRequired,
+};
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -4,16 +4,24 @@ import PropTypes from 'prop-types';
 
 type FeedbackListProps = {
     feedback: any;
+    handleDelete: number;
 };
 
-export default function FeedbackList({ feedback }: FeedbackListProps) {
+export default function FeedbackList({
+    feedback,
+    handleDelete,
+}: FeedbackListProps) {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet.</p>;
     } else {
         return (
             <div className='feedback-list'>
                 {feedback.map((item: any) => (
-                    <FeedbackItem key={item.id} item={item} />
+                    <FeedbackItem
+                        key={item.id}
+                        item={item}
+                        handleDelete={handleDelete}
+                    />
                 ))}
             </div>
         );
