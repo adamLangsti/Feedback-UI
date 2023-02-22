1cf73d6d6c6527b5c837d8d1a533706f8f729249
Adam Lang <adam.lang-1@hotmail.com>
2023-02-22 19:49:27 +0200

    feat: function passed to button onClick prop enabling item edit

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import FeedbackContext from '../context/FeedbackContext';
 
 export default function FeedbackItem({ item }) {
-    const { deleteFeedback }: any = useContext(FeedbackContext);
+    const { deleteFeedback, editFeedback }: any = useContext(FeedbackContext);
+
     return (
         <Card>
             <div className='num-display'>{item.rating}</div>
             <button onClick={() => deleteFeedback(item.id)} className='close'>
                 <FaTimes color='purple' />
+            </button>
+            <button onClick={() => editFeedback(item)} className='edit'>
+                <FaEdit color='purple' />
             </button>
             <div className='text-display'>{item.text}</div>
         </Card>
